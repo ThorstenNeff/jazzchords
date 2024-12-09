1b90a867b3c4bfdddfd4a9da1699dbbab11fa619
thorstenneff <thorstenneff@gmx.de>
2024-12-09 20:16:14 +0100

    Fix theme

--- app/src/main/java/com/neffapps/jazzchords/ui/theme/Theme.kt
+++ app/src/main/java/com/neffapps/jazzchords/ui/theme/Theme.kt
@@ -1,15 +1,14 @@
 package com.neffapps.jazzchords.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
-    primaryVariant = Purple700,
     secondary = Teal200,
 
     background = Color.White,
@@ -20,9 +19,8 @@ private val DarkColorPalette = darkColors(
     onSurface = Color.Black,
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Purple500,
-    primaryVariant = Purple700,
     secondary = Teal200,
 
     background = Color.White,
@@ -42,7 +40,7 @@ fun JazzchordsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     }
 
     MaterialTheme(
-            colors = colors,
+            colorScheme = colors,
             typography = Typography,
             shapes = Shapes,
             content = content
